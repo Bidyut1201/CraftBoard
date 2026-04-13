6f9b3e8234c4872f1d71b66a2e5671f776a7e71d
Bidyutprava Biswal <bidyutprava1201@gmail.com>
2026-04-14 00:44:54 +0530

    ready for deployment

--- Backend/src/server.js
+++ Backend/src/server.js
@@ -1,6 +1,7 @@
 import express from "express";
 import cors from "cors"
 import dotenv from "dotenv";
+import path from "path"
 
 import notesRoutes from "../src/routes/notesRoutes.js"
 import { connectDB } from "./config/db.js";
@@ -10,17 +11,34 @@ import rateLimiter from "./middleware/rateLimiter.js";
 dotenv.config();
 
 const app = express();
+const __dirname = path.resolve()
 
 
 
 //middleware
-app.use(cors())
+if(process.env.NODE_ENV !== "production"){
+    app.use(
+        cors({
+            origin: "http://localhost:5173"
+        })
+    );
+}
+
 app.use(express.json())
 app.use(rateLimiter)
 
 
 
 app.use("/api/notes", notesRoutes);
+
+if(process.env.NODE_ENV === "production"){
+    app.use(express.static(path.join(__dirname,"../Frontend/dist")))
+
+    app.get("*",(req,res) => {
+        res.sendFile(path.join(__dirname,"../Frontend","dist","index.html"))
+    })
+}
+
 
 connectDB().then(() => {
     app.listen(5001, () => {
